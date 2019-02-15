133aaa659b0dedc218bd95bed25543c7bfedbb9d
Victoria B <viktorija.bourassa@gmail.com>
2019-02-15 22:33:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,6 +98,15 @@ def self.first_X_students_in_grade_10(num)
 end
 
 def self.first_student_in_grade_10
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE grade = 10
+  SQL
+
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
 end
 
 def self.all_students_in_grade_X

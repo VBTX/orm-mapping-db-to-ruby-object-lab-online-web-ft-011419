9272bbc4d929c387bcc271bd3f93dfbfaa8a221c
Victoria B <viktorija.bourassa@gmail.com>
2019-02-15 22:18:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,5 +74,14 @@ class Student
 end
 
 def self.students_below_12th_grade
-  
+end
+
+def self.first_X_students_in_grade_10
+end
+
+def self.first_student_in_grade_10
+end
+
+def self.all_students_in_grade_X
+end
 end

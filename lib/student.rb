86ae03d96b28922474fa6a30b74f83a231c0b2db
Victoria B <viktorija.bourassa@gmail.com>
2019-02-15 22:29:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,8 +81,9 @@ def self.students_below_12th_grade
   WHERE grade < 12
   SQL
 
-  what = DB[:conn].execute(sql)
-  what
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end
 end
 
 def self.first_X_students_in_grade_10
